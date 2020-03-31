2abccd9eb1e57b4ea6fb535658f49fb6ba317474
ywqzzy <592838129@qq.com>
2020-03-31 21:20:09 +0800

    2020/3/31-1

---
+++ src/linkedlist/两个链表的第一个公告节点.java
@@ -0,0 +1,5 @@
+package linkedlist;
+
+public class 两个链表的第一个公告节点 {
+
+}
---
+++ src/linkedlist/删除链表的节点.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class 删除链表的节点 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while(cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if(cur != null) pre.next = cur.next;
+        return head;
+    }
+
+}
