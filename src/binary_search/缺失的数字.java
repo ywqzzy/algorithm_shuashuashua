c05fac0514991f64b88f92e8a2358c749e9e58ea
ywqzzy <592838129@qq.com>
2020-04-06 11:36:46 +0800

    2020/4/06-2

---
+++ src/binary_search/缺失的数字.java
@@ -0,0 +1,14 @@
+package binary_search;
+
+public class 缺失的数字 {
+    public int missingNumber(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == mid) left = mid + 1;
+            else right = mid-1;
+        }
+        return left;
+    }
+
+}
---
+++ src/dp/坐标型dp/轰炸敌人.java
@@ -0,0 +1,6 @@
+package dp.坐标型dp;
+
+public class 轰炸敌人 {
+    // 四个方向 贼牛逼
+
+}
